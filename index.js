60aed8d4b18027443aea35964c12920fc34c7d10
Fábio <fabio@parceiro.com.br>
2024-05-20 21:16:26 -0400

    adicionei um laço para mostrar opcoes da historia

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
-console.log("inicio do projeto");
+const history = {
+  event: "Você está em uma floresta escura, o que você faz?",
+  options: [
+    {
+      text: "Explorar a floresta",
+      next: "encontro",
+    },
+    {
+      text: "Subir em uma arvore",
+      next: "copaDaArvore",
+    },
+    {
+      text: "Pular da arvore",
+      next: "pular",
+    },
+  ],
+  encontro: {
+    event: "Voce encontra um Ogro. o que voce faz?",
+    options: [
+      {
+        text: "Correr, gritando de medo",
+        next: "fuga",
+      },
+      {
+        text: "Lutar contra o Ogro maldito",
+        next: "lutar",
+      },
+    ],
+  },
+  copaDaArvore: {
+    event: "Voce observa ao redor. O que voce ve?",
+    options: [
+      {
+        text: "Uma cabana abandonada",
+        next: "cabana",
+      },
+      {
+        text: "Uma ponte sobre um rio",
+        next: "rio",
+      },
+    ],
+  },
+  pular: {
+    final:
+      "A arvore é bem alta e voce caiu em cima de uma pedra. adeus querido",
+  },
+};
+
+let eventoAtual = history;
+
+while (eventoAtual) {
+  if (eventoAtual.event) {
+    console.log(eventoAtual.event);
+  } else {
+    console.log(eventoAtual.final);
+  }
+
+  if (eventoAtual.options) {
+    for (let contador = 0; contador < eventoAtual.options.length; contador++) {
+      console.log(contador + ". " + eventoAtual.options[contador].text);
+    }
+  } else {
+    console.log("Fim da história");
+  }
+
+  break;
+}
