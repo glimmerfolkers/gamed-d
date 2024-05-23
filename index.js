bee7df9f700fd7354fdcc4fdf67fb7683394a0a0
driana08 <adrianavaldesprado08@gmail.com>
2024-05-23 19:19:03 -0400

    Continuar historia, ainda falta

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ const history = {
       next: "copaDaArvore",
     },
     {
-      text: "Pular da arvore",
-      next: "pular",
+      text: "Procurar um chaminho para casa",
+      next: "cidade",
     },
   ],
   encontro: {
-    event: "Voce encontra um Ogro. o que voce faz?",
+    event: "De repente um Ogro aparece atras de você, o que voce faz?",
     options: [
       {
         text: "Correr, gritando de medo",
@@ -40,10 +40,75 @@ const history = {
       },
     ],
   },
-  pular: {
+  cidade: {
     final:
-      "A arvore é bem alta e voce caiu em cima de uma pedra. adeus querido",
+      "Você acha a cidade e volta para casa",
   },
+  fuga: {
+    event: "Você tenta fugir, mas o Ogro é mais rápido. O que você faz?",
+    options: [
+      {
+        text: "Procura uma lugar para se esconder",
+        next: "Esconderijo",
+      },
+      {
+        text: "Você desiste e deixa ser pego pelo Ogro",
+        next: "desistir",
+      },
+      {
+        text: "Você tenta correr mais rápido",
+        next: "corrida",
+      }
+    ]
+  },
+  lutar: {
+    event: "Você olha ao redor em busca de uma arma improvisada, o que você pega?",
+    options: [
+      {
+        text: "Pedaço de madeira podre",
+        next: "madeira",
+      },
+      {
+        text: "Pedra muito rígida de tamanho médio",
+        next: "pedra",
+      },
+      {
+        text: "Meias de proxedência duvidosa",
+        next: "meias",
+      },
+    ]
+  },
+  cabana: {
+    event: "Você desce da árvore e vai em direção a cabana, qual seu próximo passo?",
+    options: [
+      {
+        text: "Explorar a cabana",
+        next: "explorar"
+      },
+      {
+        text: "Ignorar a cabana e seguir em direção da ponte",
+        next: "rio"
+      },
+    ]
+  },
+  rio: {
+    event: "Você segue em direção ao rio e percebe a ponte quebrada, o que você faz?",
+    options: [
+      {
+        text: "Passar pelo rio nadando, adoro um desafio!",
+        next: "nadar"
+      },
+      {
+        text: "Investigar a ponte usando minhas habilidades de detetive",
+        next: "investigar",
+      },
+      {
+        text: "Ficar quieto e pensar",
+        next: "encontro",
+      },
+    ]
+  }
+
 };
 
 // continuar a historia com mais opções de acontecimentos.
