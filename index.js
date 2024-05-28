cb68d11ba14faef6e4e212f0033295fb090a0a64
Fábio <fabio@parceiro.com.br>
2024-05-27 21:07:25 -0400

    adicionei a tela para conseguir controlar os eventos

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const history = {
   event: "Você está em uma floresta escura, o que você faz?",
+  image: "assets/animations/dark-forest.jpeg",
   options: [
     {
       text: "Explorar a floresta",
@@ -16,6 +17,7 @@ const history = {
   ],
   encontro: {
     event: "De repente um Ogro aparece atras de você, o que voce faz?",
+    image: "assets/animations/ogro.gif",
     options: [
       {
         text: "Correr, gritando de medo",
@@ -41,15 +43,14 @@ const history = {
     ],
   },
   cidade: {
-    final:
-      "Você acha a cidade e volta para casa",
+    final: "Você acha a cidade e volta para casa",
   },
   fuga: {
     event: "Você tenta fugir, mas o Ogro é mais rápido. O que você faz?",
     options: [
       {
         text: "Procura uma lugar para se esconder",
-        next: "Esconderijo",
+        next: "esconderijo",
       },
       {
         text: "Você desiste e deixa ser pego pelo Ogro",
@@ -58,11 +59,12 @@ const history = {
       {
         text: "Você tenta correr mais rápido",
         next: "corrida",
-      }
-    ]
+      },
+    ],
   },
   lutar: {
-    event: "Você olha ao redor em busca de uma arma improvisada, o que você pega?",
+    event:
+      "Você olha ao redor em busca de uma arma improvisada, o que você pega?",
     options: [
       {
         text: "Pedaço de madeira podre",
@@ -76,27 +78,30 @@ const history = {
         text: "Um espelho mediano",
         next: "espelho",
       },
-    ]
+    ],
   },
   cabana: {
-    event: "Você desce da árvore e vai em direção a cabana, qual seu próximo passo?",
+    event:
+      "Você desce da árvore e vai em direção a cabana, qual seu próximo passo?",
     options: [
       {
         text: "Explorar a cabana",
-        next: "explorar"
+        next: "explorar",
       },
       {
         text: "Ignorar a cabana e seguir em direção da ponte",
-        next: "rio"
+        next: "rio",
       },
-    ]
+    ],
   },
   rio: {
-    event: "Você segue em direção ao rio e percebe a ponte quebrada, o que você faz?",
+    event:
+      "Você segue em direção ao rio e percebe a ponte quebrada, o que você faz?",
+    image: "assets/animations/river.jpeg",
     options: [
       {
         text: "Passar pelo rio nadando, adoro um desafio!",
-        next: "nadar"
+        next: "nadar",
       },
       {
         text: "Investigar a ponte usando minhas habilidades de detetive",
@@ -106,110 +111,125 @@ const history = {
         text: "Ficar quieto e pensar",
         next: "encontro",
       },
-    ]
+    ],
   },
-  Esconderijo: {
+  esconderijo: {
     event: "Você busca ao seu redor um lugar para se esconder, qual é?",
     options: [
       {
         text: "Atrás de uma árvore grande",
-        next: "arvoreGrande"
+        next: "arvoreGrande",
       },
       {
         text: "No topo de uma árvore",
-        next: "copaDaArvore"
+        next: "copaDaArvore",
       },
-    ]
+    ],
   },
   desistir: {
-    final: "Você é pego, e vira o jantar do Ogro"
+    final: "Você é pego, e vira o jantar do Ogro",
   },
   corrida: {
-    final: "Você foge do Ogro e consegue achar o caminho para a casa"
+    final: "Você foge do Ogro e consegue achar o caminho para a casa",
   },
   madeira: {
-    final: "Você bate no Ogro com a madeira, mas isso só o deixa mais furioso. Você morreu."
+    final:
+      "Você bate no Ogro com a madeira, mas isso só o deixa mais furioso. Você morreu.",
   },
   pedra: {
     event: "Você joga a pedra e derrota o Ogro. Qual seu próximo passo?",
     options: [
       {
         text: "Continuar explorando a floresta",
-        next: "continuar"
+        next: "continuar",
       },
       {
         text: "Tentar achar um caminho para casa",
-        next: "cidade"
-      }
-    ]
+        next: "cidade",
+      },
+    ],
   },
   espelho: {
-    event: "Você aponta o espelho para o Ogro e ele se assusta com a própria aparência. Qual seu próximo passo?",
+    event:
+      "Você aponta o espelho para o Ogro e ele se assusta com a própria aparência. Qual seu próximo passo?",
     options: [
       {
         text: "Continuar explorando a floresta",
-        next: "continuar"
+        next: "continuar",
       },
       {
         text: "Tentar achar um caminho para casa",
-        next: "cidade"
-      }
-    ]
+        next: "cidade",
+      },
+    ],
   },
   explorar: {
-    final: "Dentro da cabana você encontra uma bruxa. Ela joga veneno e você morre"
+    final:
+      "Dentro da cabana você encontra uma bruxa. Ela joga veneno e você morre",
   },
   nadar: {
-    final: "No rio há jacarés, você morreu"
+    final: "No rio há jacarés, você morreu",
   },
   investigar: {
-    event: "Você percbe que o que destruiu a ponte foi uma criatura gigante não humana, o que você faz?",
+    event:
+      "Você percebe que o que destruiu a ponte foi uma criatura gigante não humana, o que você faz?",
+    image: "assets/animations/bridge.jpeg",
     options: [
-      { 
+      {
         text: "Continuar explorando a floresta",
-        next: "continuar"
+        next: "continuar",
       },
       {
         text: "Ficar quieto e pensar",
-        next: "encontro"
+        next: "encontro",
       },
       {
         text: "Tentar achar um caminho para casa",
-        next: "cidade"
-      }
-    ]
+        next: "cidade",
+      },
+    ],
   },
   continuar: {
-    final: "Você acaba adentrando demais na floresta e perceb que é um laberinto. Você está preso para sempre"
-  }
+    final:
+      "Você acaba adentrando demais na floresta e perceb que é um laberinto. Você está preso para sempre",
+  },
 };
-
-// continuar a historia com mais opções de acontecimentos.
 
 let eventoAtual = history;
 
-while (eventoAtual) {
+const eventElement = document.getElementById("event");
+const actionsElement = document.getElementById("actions");
+const imageElement = document.getElementById("image");
+
+function showEvent() {
+  imageElement.src = eventoAtual.image;
+
   if (eventoAtual.event) {
-    console.log(eventoAtual.event);
+    eventElement.innerHTML = eventoAtual.event;
   } else {
-    console.log(eventoAtual.final);
+    eventElement.innerHTML = eventoAtual.final;
   }
-
-  if (eventoAtual.options) {
-    for (let contador = 0; contador < eventoAtual.options.length; contador++) {
-      console.log(contador + ". " + eventoAtual.options[contador].text);
-    }
-    const frase = "Leve a sério! Isso não é só um jogo!! Escolha uma opção";
-    const acao = parseInt(prompt(frase));
-
-    eventoAtual = history[eventoAtual.options[acao].next]
-    console.log(eventoAtual)
-
-  } else {
-    console.log("Fim da história");
-    break;
-  }
-
-
-
 }
+
+function showActions() {
+  actionsElement.innerHTML = "";
+
+  for (let contador = 0; contador < eventoAtual.options.length; contador++) {
+    const option = eventoAtual.options[contador];
+
+    const optionElement = document.createElement("button");
+    optionElement.classList.add("btn");
+    optionElement.innerHTML = option.text;
+
+    optionElement.addEventListener("click", () => {
+      eventoAtual = history[option.next];
+      showEvent();
+      showActions();
+    });
+
+    actionsElement.appendChild(optionElement);
+  }
+}
+
+showEvent();
+showActions();
